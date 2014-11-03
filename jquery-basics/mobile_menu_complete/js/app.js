9e526398044ebc0bb3b4ab6ea3d5eeff2fc3f8c4
alexfriant <alex@geospatialsolutions.com>
2014-11-03 14:26:09 -0800

    Completed mobile menu selector
    This version has the “Go” button

--- jquery-basics/mobile_menu_complete/js/app.js
+++ jquery-basics/mobile_menu_complete/js/app.js
@@ -1,15 +1,36 @@
 //Problem: It look gross in smaller browser widths and small devices
 //Solution: To hide the text links and swap them out with a more appropriate navigation
 
-//Create a select and append to menu
-//Cycel over menu links
+//Create a select and append to #menu
+var $select = $("<select></select>");
+$("#menu").append($select);
+
+//Cycle over menu links
+$("#menu a").each(function() {
+  var $anchor = $(this);
   //Create an option
+  var $option = $("<option></option>");
+  
+  //Deal with selected option depending on current page
+  if($anchor.parent().hasClass("selected")) {
+    $option.prop("selected", true);
+  }
+  
   //Option's value is the href
+  $option.val($anchor.attr("href"));
   //Option's text is the text of link
+  $option.text($anchor.text());
   //Append option to select
+  $select.append($option);
+})
+
 //Create button
+var $button = $("<button>Go</button>");
+$("#menu").append($button);
 //Bind click to button
+$button.click(function(){
   //Go to select's location
-//Modify CSS to hide links on small screen widths
-  //Also hides select and button on larger screen widths
-//Deal with selected option depending on current page
+  window.location = $select.val();
+});
+  
+
