da2e14381332555023b50b6559e8ba30ecca2aef
alexfriant <alex@geospatialsolutions.com>
2014-11-10 14:01:25 -0800

    Pseudo code for planning out simple drawing app
    From a jquery course

---
+++ jquery-basics/simple_drawing_complete/js/app.js
@@ -0,0 +1,20 @@
+//Problem: No user interaction causes no change to application
+//Solution: When user interacts cause changes appropriately
+
+
+//When clicking on control list items
+  //Deselect sibling elements
+  //Select clicked element
+
+//When New Color button is pressed
+  //Show color select
+
+//When the color sliders are moved
+  //Update the color span 
+
+//When Add Color button is pressed
+  //Append the color to the controls ul
+  //Select the new color just added
+
+//With mouse button pressed inside canvas
+  //Draw colors on the canvas
