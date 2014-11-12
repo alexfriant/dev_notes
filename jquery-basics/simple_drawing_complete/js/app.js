30d800931391dabe6a2f6a2531b58f77df1e2f5a
alexfriant <alex@geospatialsolutions.com>
2014-11-12 13:12:34 -0800

    got the color sliders functioning, set body height
    This is for the simple drawing app course

--- jquery-basics/simple_drawing_complete/js/app.js
+++ jquery-basics/simple_drawing_complete/js/app.js
@@ -10,14 +10,37 @@ $(".controls li").click(function() {
   $(this).addClass("selected");
   //cache current color here
   color = $(this).css("background-color")
-})
+});
 
 
 //When New Color button is pressed
-  //Show color select
+$("#revealColorSelect").click(function() {
+  //Update color swatch
+  changeColor();
+  //Show or hide color select
+  $("#colorSelect").toggle();
+});
 
-//When the color sliders are moved
-  //Update the color span 
+//return an RGB formatted color value from the sliders
+function slidersRGB(){
+  //get the slider bars values
+  var r = $("#red").val();
+  var g = $("#green").val();
+  var b = $("#blue").val();
+  
+  //return the color variable
+  return("rgb(" + r + "," + g + "," + b + ")"); 
+};
+
+//Update the color span function
+function changeColor() {  
+  //set the span color
+  $("#newColor").css("background-color", slidersRGB());
+};
+
+//When the color sliders are moved change the color of the sample color swatch
+$("input[type=range]").change(changeColor);
+
 
 //When Add Color button is pressed
   //Append the color to the controls ul
