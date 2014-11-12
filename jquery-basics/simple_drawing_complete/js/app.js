335d5ba093213e060c84bc15e778cd476b86f7ab
alexfriant <alex@geospatialsolutions.com>
2014-11-12 12:24:08 -0800

    color button controls working
    This is for the simple drawing page course

--- jquery-basics/simple_drawing_complete/js/app.js
+++ jquery-basics/simple_drawing_complete/js/app.js
@@ -1,10 +1,17 @@
 //Problem: No user interaction causes no change to application
 //Solution: When user interacts cause changes appropriately
-
+var color = $(".selected").css("background-color");
 
 //When clicking on control list items
+$(".controls li").click(function() {
   //Deselect sibling elements
+  $(this).siblings().removeClass("selected");
   //Select clicked element
+  $(this).addClass("selected");
+  //cache current color here
+  color = $(this).css("background-color")
+})
+
 
 //When New Color button is pressed
   //Show color select
