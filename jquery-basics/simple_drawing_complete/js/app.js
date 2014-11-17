7b9da767f3948cae919227eead82dae918b58636
alexfriant <alex@geospatialsolutions.com>
2014-11-17 13:57:50 -0800

    You can create new colors, and draw with color
    MVP for drawing

--- jquery-basics/simple_drawing_complete/js/app.js
+++ jquery-basics/simple_drawing_complete/js/app.js
@@ -1,9 +1,13 @@
 //Problem: No user interaction causes no change to application
 //Solution: When user interacts cause changes appropriately
 var color = $(".selected").css("background-color");
+var $canvas = $("canvas");
+var context = $canvas[0].getContext("2d");
+var lastEvent;
+var mouseDown = false;
 
 //When clicking on control list items
-$(".controls li").click(function() {
+$(".controls").on("click", "li", function() {
   //Deselect sibling elements
   $(this).siblings().removeClass("selected");
   //Select clicked element
@@ -13,7 +17,7 @@ $(".controls li").click(function() {
 });
 
 
-//When New Color button is pressed
+//When "New Color" button is pressed
 $("#revealColorSelect").click(function() {
   //Update color swatch
   changeColor();
@@ -42,9 +46,32 @@ function changeColor() {
 $("input[type=range]").change(changeColor);
 
 
-//When Add Color button is pressed
+//When "Add Color" button is pressed
+$("#addNewColor").click(function() {
   //Append the color to the controls ul
+  var $newColor = $("<li></li>");
   //Select the new color just added
+  $newColor.css("background-color", $("#newColor").css("background-color"));
+  $(".controls ul").append($newColor);
+  $newColor.click();
+});
+
 
 //With mouse button pressed inside canvas
+$canvas.mousedown(function(e) {
+  lastEvent = e;
+  mouseDown = true;
+}).mousemove(function(e) {
+  if(mouseDown) {
+  context.beginPath();
+  context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
+  context.lineTo(e.offsetX, e.offsetY);
+  context.strokeStyle = color;
+  context.stroke();
+  lastEvent = e;
+  };
+}).mouseup(function(){
+  mouseDown = false;
+});
+
   //Draw colors on the canvas
