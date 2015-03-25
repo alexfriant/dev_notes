62f098f99fad3c8c9ef45d3653bb54a149fafb33
alexfriant <alex.friant@gmail.com>
2015-03-25 15:39:59 -0700

    array methods, course challenge

--- js-loops-arrays-objects/Section_2_Arrays/video06/js/search.js
+++ js-loops-arrays-objects/Section_2_Arrays/video06/js/search.js
@@ -5,3 +5,17 @@ function print(message) {
   document.write( '<p>' + message + '</p>');
 }
 
+while ( true ) {
+  search = prompt("Search for a product in our store. Type 'list' to show all of the produce. Type 'quit' to exit the search.").trim().toLowerCase();
+  if ( search === 'quit' || search === 'exit' ) {
+    break;
+  } else if ( search === 'list') {
+    print(inStock.join(', '));
+  } else if ( search !== '' ) {
+    if ( inStock.indexOf( search ) === -1 ) {
+      print("Sorry, we don't have " + search + " in stock.");
+    } else {
+      print("Yes, we have " + search + " in stock.");
+    }
+  }
+}
